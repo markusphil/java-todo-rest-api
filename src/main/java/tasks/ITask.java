fce43adf1996181f5466c6952a681e4905b5cdaa
markusphil <m.philipp1991@web.de>
2020-10-03 13:25:18 +0200

    add optional Task properties

--- src/main/java/tasks/ITask.java
+++
@@ -1,7 +0,0 @@
-package tasks;
-
-public interface ITask {
-    void setStatus(boolean isDone);
-    void setPriority(int priorityNumber);
-    void setCategory(Category category);
-}
--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -1,17 +1,26 @@
 package tasks;
 
+import java.sql.Date;
+
 public class Task {
     public String name;
     public int id;
     public Category category;
+    public String description;
+    public Date createdAt;
+    public Date dueTo;
 
     public Task(String name){
         this.name = name;
     }
 
-    public void setId(int id){
-        this.id = id;
-    }
+    public void setId(int id){ this.id = id; }
+
+    public void setCreatedAt(Date createdDate){ this.createdAt = createdDate; }
+
+    public void setDueTo(Date dueDate){ this.dueTo = dueDate; }
+
+    public void setDescription(String description){ this.description = description; }
 
     public void setCategory(Category cat) { this.category = cat; }
 }
